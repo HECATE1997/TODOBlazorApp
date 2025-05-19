11fafa6b44e4dfa658545ef0a9b5fc3615ba3824
Bijayash Chitrakar <chitrakar.bijayash@dishhome.com.np>
2025-05-20 00:21:22 +0545

    initial commit

---
+++ TodoAppBlazorServer/TodoAppBlazorServer/Services/TodoService.cs
@@ -0,0 +1,38 @@
+﻿namespace TodoAppBlazorServer.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IList<TodoItem> _todoItems = new List<TodoItem>();
+        public TodoService()
+        {
+            _todoItems = new List<TodoItem>
+            {
+                new TodoItem("Learn Blazor"),
+                new TodoItem("Build a Blazor app"),
+                new TodoItem("Deploy the app")
+            };
+        }
+        public void Add(TodoItem item)
+        {
+            _todoItems.Add(item);
+        }
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoItems.ToList();
+        }
+        public void Delete(TodoItem item)
+        {
+            _todoItems.Remove(item);
+        }
+
+        public void Complete(TodoItem item)
+        {
+            item.Completed = true;
+        }
+
+        public void Uncomplete(TodoItem item)
+        {
+            item.Completed = false;
+        }
+    }
+}
